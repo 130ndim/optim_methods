dedf3fbe76a0de2ba29a6f44321099c45093421d
Dmitry <130ndim@gmail.com>
2020-05-13 22:11:49 +0300

    logreg example

--- descent.py
+++ descent.py
@@ -18,47 +18,52 @@ def line_search(fn, x0, step=0.01, multiplier=1.5):
 def gradient_descent(fn, fn_grad, x0, alpha=1e-3, eps=1e-6, compute_alpha=False,
                      verbose=False, method='line', **kwargs):
     curr_x = x0
-    curr_grad = fn_grad(x0)
+    crit = np.linalg.norm(fn_grad(curr_x)) * eps
     i = 0
     while True:
         i += 1
+        curr_grad = fn_grad(curr_x)
+        grad_norm = np.linalg.norm(curr_grad)
         if verbose and i % verbose == 0:
             print(
-                f'Iteration {i}, Grad norm = {np.linalg.norm(curr_grad):.6f}')
+                f'Iteration {i}, Grad norm: {grad_norm:.6f}, '
+                f'Left: {grad_norm - crit:.6f}')
         if compute_alpha:
             alpha = optimize_step(fn, curr_grad, curr_x, method=method, **kwargs)
-        curr_x = curr_x - alpha * curr_grad
-        curr_grad = fn_grad(curr_x)
-        curr_norm = np.linalg.norm(curr_grad)
-        if np.abs(curr_norm) < eps:
+        if np.abs(grad_norm) < crit:
             return curr_x
+        curr_x = curr_x - alpha * curr_grad
 
 
-def optimize_step(fn, grad, x0, method='line', **kwargs):
+def optimize_step(fn, grad, x0, method='line', confidence=1e-6, **kwargs):
     objective = lambda x: fn(x0 - x * grad)
     if method == 'line':
         return line_search(objective, np.random.rand(), **kwargs)
     else:
-        return UnidimSearch(method)(objective, 0, 1, **kwargs)['min']
+        return UnidimSearch(method)(objective, 0, 1, eps=confidence, **kwargs)['min']
 
 
 def newton(fn_grad, fn_hess, x0, eps=1e-6, verbose=False):
     curr_x = x0
     i = 0
+    crit = np.linalg.norm(fn_grad(curr_x)) * eps
     while True:
         i += 1
         curr_grad = fn_grad(curr_x)
         curr_hess = fn_hess(curr_x)
+
         # if Hessian is positive definite use Cholesky decomposition
         if np.all(eigvals(curr_hess) > 0):
             d = cho_solve(cho_factor(curr_hess), curr_grad)
         else:
             d = np.linalg.inv(curr_hess).dot(curr_grad)
+
+        grad_norm = np.linalg.norm(curr_grad)
+
         if verbose and i % verbose == 0:
             print(
-                f'Iteration {i}, Step norm = {np.linalg.norm(d):.6f}')
-        if np.linalg.norm(d) < eps:
+                f'Iteration {i}, Grad norm: {grad_norm:.6f}, Left: {max(0, grad_norm - eps):.6f}')
+        if grad_norm < crit:
             return curr_x
         curr_x = curr_x - d
-
 
