5201d00f438b0084343097808d2fee629ee9642a
Dmitry <130ndim@gmail.com>
2020-05-13 21:00:03 +0300

    Minor GD fix

--- descent.py
+++ descent.py
@@ -19,23 +19,19 @@ def gradient_descent(fn, fn_grad, x0, alpha=1e-3, eps=1e-6, compute_alpha=False,
                      verbose=False, method='line', **kwargs):
     curr_x = x0
     curr_grad = fn_grad(x0)
-    prev_norm = np.linalg.norm(curr_grad)
     i = 0
     while True:
         i += 1
         if verbose and i % verbose == 0:
             print(
-                f'Iteration {i}, Norm difference = {np.linalg.norm(curr_grad):.6f}')
+                f'Iteration {i}, Grad norm = {np.linalg.norm(curr_grad):.6f}')
         if compute_alpha:
             alpha = optimize_step(fn, curr_grad, curr_x, method=method, **kwargs)
         curr_x = curr_x - alpha * curr_grad
         curr_grad = fn_grad(curr_x)
         curr_norm = np.linalg.norm(curr_grad)
-        if np.abs(prev_norm - curr_norm) < eps:
-            break
-        else:
-            prev_norm = curr_norm
-    return curr_x
+        if np.abs(curr_norm) < eps:
+            return curr_x
 
 
 def optimize_step(fn, grad, x0, method='line', **kwargs):
