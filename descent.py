25a9ba231968eeabdcab686aee7b30eb8c03adcf
Dmitry <130ndim@gmail.com>
2020-05-30 04:14:57 +0300

    trajectory

--- descent.py
+++ descent.py
@@ -16,10 +16,11 @@ def line_search(fn, x0, step=0.01, multiplier=1.5):
 
 
 def gradient_descent(fn, fn_grad, x0, alpha=1e-3, eps=1e-6, compute_alpha=False,
-                     verbose=False, method='line', **kwargs):
+                     verbose=False, method='line', return_trajectory=False, **kwargs):
     curr_x = x0
     crit = np.linalg.norm(fn_grad(curr_x)) * eps
     i = 0
+    trajectory = [curr_x]
     while True:
         i += 1
         curr_grad = fn_grad(curr_x)
@@ -31,8 +32,12 @@ def gradient_descent(fn, fn_grad, x0, alpha=1e-3, eps=1e-6, compute_alpha=False,
         if compute_alpha:
             alpha = optimize_step(fn, curr_grad, curr_x, method=method, **kwargs)
         if np.abs(grad_norm) < crit:
-            return curr_x
+            if return_trajectory:
+                return trajectory
+            else:
+                return curr_x
         curr_x = curr_x - alpha * curr_grad
+        trajectory.append(curr_x)
 
 
 def optimize_step(fn, grad, x0, method='line', confidence=1e-6, **kwargs):
@@ -43,10 +48,11 @@ def optimize_step(fn, grad, x0, method='line', confidence=1e-6, **kwargs):
         return UnidimSearch(method)(objective, 0, 1, eps=confidence, **kwargs)['min']
 
 
-def newton(fn_grad, fn_hess, x0, eps=1e-6, verbose=False):
+def newton(fn_grad, fn_hess, x0, eps=1e-6, verbose=False, return_trajectory=False):
     curr_x = x0
     i = 0
     crit = np.linalg.norm(fn_grad(curr_x)) * eps
+    trajectory = [curr_x]
     while True:
         i += 1
         curr_grad = fn_grad(curr_x)
@@ -64,6 +70,10 @@ def newton(fn_grad, fn_hess, x0, eps=1e-6, verbose=False):
             print(
                 f'Iteration {i}, Grad norm: {grad_norm:.6f}, Left: {max(0, grad_norm - eps):.6f}')
         if grad_norm < crit:
-            return curr_x
+            if return_trajectory:
+                return trajectory
+            else:
+                return curr_x
         curr_x = curr_x - d
+        trajectory.append(curr_x)
 
